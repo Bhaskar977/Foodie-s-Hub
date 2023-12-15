97c7c24c3fb41fa1142e62a46d4e3175066ae45c
Bhaskar977 <bhaskerjoshi977@gmail.com>
2023-12-15 23:35:20 +0530

    done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import Card from '../components/Card'
 import Footer from '../components/Footer'
 import Carousel from '../components/Carousel'
 
 const Home = () => {
+
+    const [foodCategory, setFoodCategory] = useState([])
+    const [foodItems, setFoodItems] = useState([])
+
+    const loadData = async () => {
+        let response = await fetch("http://localhost:8000/api/fooddata", {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+        response = await response.json()
+        setFoodItems(response[0])
+        setFoodCategory(response[1])
+    }
+
+    useEffect(() => {
+        loadData()
+    }, [])
+
     return (
         <>
             <div><Navbar /></div>
             <div>
                 <Carousel />
             </div>
-            <div>
-                <Card />
+            <div className='container'>
+                {
+                    foodCategory ?
+                        (
+                            foodCategory.map((data) => {
+                                return (
+                                    <div className='row mb-3'>
+                                        <div key={data.id} className='fs-3 m-3'>{data.CategoryName}</div>
+                                        <hr />
+                                        {
+                                            foodItems?(
+                                                foodItems.filter((el)=> el.CategoryName==data.CategoryName)
+                                                .map((filteritem)=>{
+                                                    // console.log(data.CategoryName,filteritem)
+                                                    return(
+                                                        <div key={filteritem._id} className='col-12 col-md-6 col-lg-3'>
+                                                            <Card name = {filteritem.name} image = {filteritem.img} />
+                                                        </div>
+                                                    )
+                                                })
+                                            ):(<div>No such Data Found</div>)
+                                        }
+                                    </div>
+                                )
+                            })
+                        ) :
+                        (
+                            ""
+                        )
+                }
             </div>
             <div><Footer /></div>
         </>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const [credentials,setCredentials] = useState({
+    email:"",password:""
+  })
+  const navigate = useNavigate();
+
+  const handleSubmit = async(e) =>{
+    e.preventDefault()
+    const response = await fetch("http://localhost:8000/api/loginuser",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        email:credentials.email,
+        password:credentials.password
+      })
+    })
+
+    const json = await response.json()
+    if(json.success){
+      localStorage.setItem("authToken",json.authToken)
+      navigate("/")
+    }
+    if(!json.success){
+      alert("Enter valid credentials")
+    }
+  }
+
+  const handleChange = (e) =>{
+    setCredentials({...credentials,[e.target.name]:e.target.value})
+  }
+
   return (
-    <div>Login</div>
+    <div className='container mt-3'>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3 row">
+          <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Email</label>
+          <div className="col-sm-6">
+            <input type="text" className="form-control" name="email" value={credentials.email} id="staticEmail" onChange={handleChange} />
+          </div>
+        </div>
+        <div className="mb-3 row">
+          <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
+          <div className="col-sm-6">
+            <input type="password" className="form-control" name="password" value={credentials.password} id="inputPassword" onChange={handleChange} />
+          </div>
+        </div>
+        <button type='submit' className='btn btn-success'>Login</button>
+        <Link to="/signup" className='m-3 btn btn-dark'>New user</Link>
+      </form>
+    </div>
   )
 }
 
